3d7677814e5052bd897daf67aca30df5143e1c9b
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-06-07 19:39:03 +0700

    make api create mentor

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -11,6 +11,11 @@ class Mentor extends Model
 
     protected $table = 'mentors';
 
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
     protected $fillable = [
         'name',
         'profile',
